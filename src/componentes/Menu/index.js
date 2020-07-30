ee34dc638af68faaa8bc01287cf2bee424ef8b4a
ThaliaDev <thalia_sara@hotmail.com>
2020-07-30 00:28:51 -0300

    Adicionando carousel

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Logo from '../../assets/logo/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom';
 //import Botao from './componentes/Botao';
 
 function Menu(){
 
     return(
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
             <img className= "Logo" src = {Logo} alt= "GamerFlix Logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" className = "ButtonLink" href = "/" >
+            <Button as={Link} className = "ButtonLink" to = "/cadastro/video" >
             Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+
+//O browserrouter vai ficar em volta de tudo e a app carrega dentro dele.
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<BrowserRouter>
+<Switch>
+  <Route path = "/" component = {Home} exact/>
+  <Route path = "/cadastro/video" component= {CadastroVideo}/>
+  <Route path = "/cadastro/categoria" component={CadastroCategoria}/>
+  <Route component ={Pagina404}/>
+</Switch>
+
+
+</BrowserRouter>,
+ // <React.StrictMode>
+ //   <App />
+ // </React.StrictMode>,
   document.getElementById('root')
 );
 

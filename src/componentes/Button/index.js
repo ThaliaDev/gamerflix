2cccf07e7b8f19d4dfea037e7cbd5eaf32b58872
ThaliaDev <thalia_sara@hotmail.com>
2020-08-04 06:41:13 -0300

    Descrição

--- src/componentes/Button/index.js
+++ src/componentes/Button/index.js
@@ -22,6 +22,29 @@ const Button = styled.button`
     &:focus {
         opacity: .5;
     }
+    @media (max-width: 800px) {
+        color: white;
+    //border: 1px solid orangered;
+    border-image: linear-gradient(to right, blue , orangered);
+    border-image-slice: 1;
+    background:black;
+    box-sizing: border-box;
+    cursor: pointer;
+    padding: 16px 24px;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 16px;
+    outline: none;
+    border-radius: 5px;
+    text-decoration: none;
+    display: inline-block;
+    transition: opacity .3s;
+    }
+    &:hover,
+    &:focus {
+        opacity: .5;
+    }
+      }
 `;// styled.a (tag que a gente passou, ou seja, a de link) | As crases são tagged template string
   // uma propriedade do JS. | o &(antes, .ButtonLink) em baixo, serve para referenciar o botão.
 
